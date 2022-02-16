90ec4909bc38ceb98db0164c0cdc36583996a064
Sorgelig <pour.garbage@gmail.com>
2022-02-16 23:30:41 +0700

    Update sys. Persistent mounted image.

--- TSConf.sv
+++ TSConf.sv
@@ -29,7 +29,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [45:0] HPS_BUS,
+	inout  [48:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        CLK_VIDEO,
@@ -55,8 +55,9 @@ module emu
 
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
+	output        HDMI_FREEZE,
 
-`ifdef USE_FB
+`ifdef MISTER_FB
 	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
 	// FB_FORMAT:
 	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
@@ -74,6 +75,7 @@ module emu
 	input         FB_LL,
 	output        FB_FORCE_BLANK,
 
+`ifdef MISTER_FB_PALETTE
 	// Palette control for 8bit modes.
 	// Ignored for other video modes.
 	output        FB_PAL_CLK,
@@ -81,6 +83,7 @@ module emu
 	output [23:0] FB_PAL_DOUT,
 	input  [23:0] FB_PAL_DIN,
 	output        FB_PAL_WR,
+`endif
 `endif
 
 	output        LED_USER,  // 1 - ON, 0 - OFF.
@@ -112,7 +115,6 @@ module emu
 	output        SD_CS,
 	input         SD_CD,
 
-`ifdef USE_DDRAM
 	//High latency DDR3 RAM interface
 	//Use for non-critical time purposes
 	output        DDRAM_CLK,
@@ -125,9 +127,7 @@ module emu
 	output [63:0] DDRAM_DIN,
 	output  [7:0] DDRAM_BE,
 	output        DDRAM_WE,
-`endif
 
-`ifdef USE_SDRAM
 	//SDRAM interface with lower latency
 	output        SDRAM_CLK,
 	output        SDRAM_CKE,
@@ -140,10 +140,10 @@ module emu
 	output        SDRAM_nCAS,
 	output        SDRAM_nRAS,
 	output        SDRAM_nWE,
-`endif
 
-`ifdef DUAL_SDRAM
+`ifdef MISTER_DUAL_SDRAM
 	//Secondary SDRAM
+	//Set all output SDRAM_* signals to Z ASAP if SDRAM2_EN is 0
 	input         SDRAM2_EN,
 	output        SDRAM2_CLK,
 	output [12:0] SDRAM2_A,
@@ -183,6 +183,7 @@ assign LED_DISK  = {1'b1, ~vsd_sel & sd_act};
 assign LED_POWER = 0;
 assign BUTTONS   = 0;
 assign VGA_SCALER= 0;
+assign HDMI_FREEZE = 0;
 
 wire [1:0] ar = status[33:32];
 wire       vcrop_en = status[34];
@@ -206,7 +207,7 @@ video_freak video_freak
 `include "build_id.v" 
 localparam CONF_STR = {
 	"TSConf;;",
-	"S,VHD,Mount virtual SD;",
+	"SC0,VHD,Mount virtual SD;",
 	"-;",
 	"o01,Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
 	"O12,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%;",
@@ -294,7 +295,6 @@ wire        sd_buff_wr;
 wire        img_mounted;
 wire        img_readonly;
 wire [63:0] img_size;
-wire        sd_ack_conf;
 wire [64:0] RTC;
 
 wire        ioctl_wr;
@@ -303,17 +303,15 @@ wire  [7:0] ioctl_dout;
 wire        ioctl_download;
 wire  [7:0] ioctl_index;
 
-hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
+hps_io #(.CONF_STR(CONF_STR)) hps_io
 (
 	.clk_sys(clk_sys),
 	.HPS_BUS(HPS_BUS),
 
-	.conf_str(CONF_STR),
-
 	.joystick_0(joy_0),
 	.joystick_1(joy_1),
-	.joystick_analog_0(joya_0),
-	.joystick_analog_1(joya_1),
+	.joystick_l_analog_0(joya_0),
+	.joystick_l_analog_1(joya_1),
 
 	.buttons(buttons),
 	.status(status),
@@ -326,14 +324,13 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.ps2_mouse(ps2_mouse),
 	.ps2_key(ps2_key),
 
-	.sd_lba(sd_lba),
+	.sd_lba('{sd_lba}),
 	.sd_rd(sd_rd),
 	.sd_wr(sd_wr),
 	.sd_ack(sd_ack),
-	.sd_ack_conf(sd_ack_conf),
 	.sd_buff_addr(sd_buff_addr),
 	.sd_buff_dout(sd_buff_dout),
-	.sd_buff_din(sd_buff_din),
+	.sd_buff_din('{sd_buff_din}),
 	.sd_buff_wr(sd_buff_wr),
 	.img_mounted(img_mounted),
 	.img_readonly(img_readonly),
@@ -468,6 +465,7 @@ video_mixer #(.GAMMA(1)) video_mixer
 	.*,
 	.scandoubler(scale || forced_scandoubler),
 	.hq2x(scale==1),
+	.freeze_sync(),
 	.VGA_DE(vga_de)
 );
 
